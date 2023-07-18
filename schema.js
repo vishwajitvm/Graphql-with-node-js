fdb4568b5afa97750cc1ff39ed52d33e2fa89af0
vishwajitvm <vishwajitmall50@gmail.com>
2023-07-18 07:41:47 +0530

    Basics of grapql - comp

--- schema.js
+++ schema.js
@@ -1,10 +1,48 @@
-const graphiql = require('graphql');
 const {
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLInt,
     GraphQLString,
     GraphQLList
-} = graphiql
+} = require('graphql'); 
+
+const UserType = new GraphQLObjectType({
+    name: 'User', 
+    fields: () => ({
+        id: { type: GraphQLInt },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLInt } 
+    })
+});
+
+// CREATE DUMMY DATA
+// Alternatively, we use a mutation to do so
+const RootQuery = new GraphQLObjectType({
+    name: 'Query', 
+    fields: {
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args) {
+                let data = [
+                    {
+                        id: 12,
+                        name: 'vishwajit',
+                        email: 'vishwa@gmail.com',
+                        phone: 89203521
+                    },
+                    {
+                        id: 14,
+                        name: 'rajesh',
+                        email: 'rajesh@gmail.com',
+                        phone: 454545412
+                    }
+                ];
+                return data;
+            }
+        }
+    }
+});
 
 
+module.exports = new GraphQLSchema({ query: RootQuery })
